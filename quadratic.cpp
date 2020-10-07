fb9071c0ceb1964828ff9308a94ba6eff4f8af09
StanislavKy <70342945+StanislavKy@users.noreply.github.com>
2020-10-07 15:26:50 +0300

    Update quadratic.cpp

--- quadratic.cpp
+++ quadratic.cpp
@@ -1,37 +1,35 @@
 #include <stdio.h>
 #include <math.h>
 
-//void CheckingInput();
+int Input();
 void SolveLinear(double a, double b, double c);
 void SolveQuadratic(double a, double b, double c);
 
 int main(){
 
-    printf("Welcome to the master of solving quadratic equations\n");
-
-    double a = 0, b = 0, c = 0;
-
-    scanf("%lf%lf%lf", a, b, c);
-
-    SolveLinear(double a, double b, double c);
-    SolveQuadratic(double a, double b, double c);
+    Input();
 
     return 0;
+
 }
 
+int Input(){
 
+    double a = 0, b = 0, c = 0, x1 = 0, x2 = 0;
 
+    printf("Welcome to the master of solving quadratic equations\n"
+           "Enter the values of the three coefficients: \n");
 
+    if(scanf("%lf%lf%lf", &a, &b, &c) != 3){
 
-/*void CheckingInput(){
+        printf("Incorrect data entered, please double-check and re-enter\n");
+        return 1;
 
-    double a = 0,
-           b = 0,
-           c = 0;
+    }
 
-    while()
+    SolveLinear(a, b, c);
 
-}  */
+}
 
 void SolveLinear(double a, double b, double c){
 
@@ -63,40 +61,68 @@ void SolveLinear(double a, double b, double c){
 
     }
 
+    else if (a != 0){
+
+        SolveQuadratic(a, b, c);
+
+    }
+
 }
 
 void SolveQuadratic(double a, double b, double c){
 
-    if(a != 0 && b == 0 && c == 0){
+    if(b == 0 && c == 0){
 
         printf("The equation one root: x = 0");
 
     }
 
-    else if(a != 0 && b != 0 && c == 0){
+    else if(b != 0 && c == 0){
 
         printf("You need to solve %lf * x^2 + %lf * x = 0\n", a, b); //axx+bx=0  x(ax + b) = 0  x = 0  x = -b/a
         printf("The equation has 2 roots: x = 0 and x = %lf\n", -b / a);
 
     }
 
-    else if(a != 0 && b == 0 && c !=0){
+    else if(b == 0 && c !=0){
 
         printf("You need to solve %lf * x^2 + %lf\n", a, c); //axx+c=0    axx= -c  xx= -c/a
         printf("The equation has no roots\n");
 
     }
 
-    else if(a != 0 && b != 0 && c != 0){
+    else if(b != 0 && c != 0){
 
         double x1 = 0, x2 = 0, D = 0;
 
         D  = b * b - 4 * a * c;
-        x1 = (-b + sqrt(D)) / (2 * a);
-        x1 = (-b - sqrt(D)) / (2 * a);
 
-        printf("The equation has 2 roots: x = %lf and x = %lf", x1, x2);
+        if(D < 0){
+
+            printf("The equation has no roots\n");
+
+        }
+
+        else if(D == 0){
+
+            printf("The equation has one root: %lf", (-b) / (2 * a));
+
+        }
+
+        else if(D > 0){
+
+            printf("You need to solve: %lf * x^2 + %lf * x + %lf = 0\n", a, b, c);
+            
+            x1 = (-b + sqrt(D)) / (2 * a);
+            
+            x1 = (-b - sqrt(D)) / (2 * a);
+            
+            printf("The equation has 2 roots: x = %lf and x = %lf", x1, x2);
+
+        }
 
     }
 
 }
+
+
